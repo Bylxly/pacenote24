c53ab1d2a5999ec1d47d8ffefd1e0c8d643b08dc
moritzcrey <moritz.a.c@web.de>
2026-06-01 11:05:58 +0200

    Update index.php

--- public/index.php
+++ public/index.php
@@ -85,9 +85,5 @@ requireAuth();
           </div>
         </div>
       </div>
-
-    </div> </div>
-
-  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
-</body>
+    </body>
 </html>

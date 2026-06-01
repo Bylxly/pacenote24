ab5fa298f2a77e623147d996e4c60900dfd1e865
moritzcrey <moritz.a.c@web.de>
2026-06-01 11:07:27 +0200

    Update and rename navigation.html to navigation.php

--- public/navigation.php
+++ public/navigation.php
@@ -1,4 +1,8 @@
 
+<?php
+require_once __DIR__ . '/../app/session/guard.php'; 
+requireAuth();
+?>
   <!DOCTYPE html>
 <html lang="de">
   <head>
@@ -7,11 +11,9 @@
     <title>Routen</title>
     
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" />
-    <link href="assets/css/stylsheetmain.css" rel="stylesheet" />
+    <link href="assets/css/stylesheetmain.css" rel="stylesheet" />
     
     <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css" />
-    
-    <script src="assets/js/homepage.js" defer></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" defer></script>
     <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"></script>
     
@@ -24,7 +26,7 @@
         <!-- Navbar-->
     <nav class="navbar navbar-expand-lg px-3">
       <div class="container-fluid">
-        <a class="navbar-brand" href="index.html">Pastenotes24.de</a>
+        <a class="navbar-brand" href="index.html">Pacenotes24.de</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navMenu">
           <span class="navbar-toggler-icon"></span>
         </button>
